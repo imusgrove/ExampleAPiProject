13d55f62dc1dcd8469e7c5b48d62b18bd7574f08
ieshaduff <iesha.musgrove@gmail.com>
2023-03-07 14:33:13 -0500

    added swagger comments

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -19,16 +19,30 @@ public class OrderController : ControllerBase
     {
         _orderService = orderService;
     }
-
+/// <summary>
+/// Creates new order
+/// </summary>
+/// <param name="order"></param>
+/// <remarks>
+///OrderType
+///{
+/// Standard = 1,
+///SaleOrder = 2,
+///PurchaseOrder = 3,
+///TransferOrder = 4,
+//ReturnOrder = 5
+///}
+/// </remarks>
+/// <returns>Newly created order</returns>
     [Route("createorder")]
     [HttpPost]
     public async Task<ActionResult<Order>> CreateOrder(Order order)
     {
         try
         {
-            var results = _orderService.CreateOrder(order);
+            var results = await _orderService.CreateOrder(order);
             Log.Information("Order created.");
-            return Ok(results);
+            return  Ok(results);
         }
         catch (Exception e)
         {
@@ -36,14 +50,18 @@ public class OrderController : ControllerBase
             throw;
         }
     }
-    
+    /// <summary>
+    /// Gets an order based on the id passed in
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns>Returns order</returns>
     [Route("getorder")]
     [HttpGet]
     public async Task<ActionResult<Order>> GetOrder(int id)
     {
         try
         {
-            var results = _orderService.GetOrder(id);
+            var results = await _orderService.GetOrder(id);
             Log.Information("Returning order ${id}.");
             return Ok(results); 
         }
@@ -54,14 +72,29 @@ public class OrderController : ControllerBase
         }
             
     }
-    
+    /// <summary>
+    /// Updates order based on id passed in
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="order"></param>
+    /// <remarks>
+    ///OrderType
+    ///{
+    /// Standard = 1,
+    ///SaleOrder = 2,
+    ///PurchaseOrder = 3,
+    ///TransferOrder = 4,
+   //ReturnOrder = 5
+    ///}
+    /// </remarks>
+    /// <returns>Returns updated order</returns>
     [Route("updateorder")]
     [HttpPut]
     public async Task<ActionResult<Order>> UpdateOrder(int id, Order order)
     {
         try
         {
-            var results = _orderService.UpdateOrder(id,order);
+            var results = await _orderService.UpdateOrder(id,order);
             Log.Information("Updating order ${id}.");
             return Ok(results);
         }
@@ -72,14 +105,28 @@ public class OrderController : ControllerBase
         }
            
     }
-    
+    /// <summary>
+    /// Search orders based on order type
+    /// </summary>
+    /// <param name="orderType"></param>
+    /// <remarks>
+    ///OrderType
+    ///{
+    /// Standard = 1,
+    ///SaleOrder = 2,
+    ///PurchaseOrder = 3,
+    ///TransferOrder = 4,
+    //ReturnOrder = 5
+    ///}
+    /// </remarks>
+    /// <returns>Returns list of orders with specified order type</returns>
     [Route("searchbyordertype")]
     [HttpGet]
     public async Task<ActionResult<Order>> SearchByOrderType(DTOS.OrderType orderType)
     {
         try
         {
-            var results = _orderService.SearchByOrderType(orderType);
+            var results = await _orderService.SearchByOrderType(orderType);
             Log.Information("Searching for ordertype ${orderType}.");
             return Ok(results); 
         }
@@ -90,7 +137,11 @@ public class OrderController : ControllerBase
         }
             
     }
-    
+    /// <summary>
+    /// Deletes order
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns>Deletes order based on id passed in</returns>
     [Route("deleteorder")]
     [HttpDelete]
     public OkObjectResult DeleteOrder(int id)
